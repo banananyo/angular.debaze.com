2357c6b80ec1d2764a1becf8b054f9f92be94910
banananyo <wazjakorn@gmail.com [credential]    helper = store>
2017-07-03 22:09:54 +0700

    add contact page

--- script/components/contact.component.js
+++ script/components/contact.component.js
@@ -1,5 +1,9 @@
 ContactController = function($scope, $sce){
     var ctrl = this;
+    $scope.imgSrc = {
+        coding2: $sce.trustAsResourceUrl('resources/images/coding2.png'),
+        coding3: $sce.trustAsResourceUrl('resources/images/coding3.png'),
+    }
 };
 
 angular.module('debazeApp').component('contact', {
